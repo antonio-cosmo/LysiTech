b164eae7eaa5ee65d033e2e4f7fe5cbd82ee12f5
Níkollas David <94586597+Ndav07@users.noreply.github.com>
2022-05-18 10:12:31 -0300

    Add avisos

--- telas_do_front/avisos.js
+++ telas_do_front/avisos.js
@@ -1,5 +1,54 @@
-const expandir = document.querySelector('.expandir');
+const expandir = document.querySelector('.expandir-icon-container');
+const expandindo = document.querySelector('.expandir-icon');
+const mural = document.querySelector('.mural');
+const divisoria = document.querySelector('.divisoria');
 
 expandir.addEventListener('click', () => {
+    mural.classList.toggle('expandindo');
+    expandindo.classList.toggle('active');
+    divisoria.classList.toggle('active');
+});
 
+const expandir2 = document.querySelector('.expandir-icon-container2');
+const expandindo2 = document.querySelector('.expandir-icon2');
+const mural2 = document.querySelector('.mural2');
+const divisoria2 = document.querySelector('.divisoria2');
+
+expandir2.addEventListener('click', () => {
+    mural2.classList.toggle('expandindo');
+    expandindo2.classList.toggle('active');
+    divisoria2.classList.toggle('active');
+});
+
+const expandir3 = document.querySelector('.expandir-icon-container3');
+const expandindo3 = document.querySelector('.expandir-icon3');
+const mural3 = document.querySelector('.mural3');
+const divisoria3 = document.querySelector('.divisoria3');
+
+expandir3.addEventListener('click', () => {
+    mural3.classList.toggle('expandindo');
+    expandindo3.classList.toggle('active');
+    divisoria3.classList.toggle('active');
+});
+
+const expandir4 = document.querySelector('.expandir-icon-container4');
+const expandindo4 = document.querySelector('.expandir-icon4');
+const mural4 = document.querySelector('.mural4');
+const divisoria4 = document.querySelector('.divisoria4');
+
+expandir4.addEventListener('click', () => {
+    mural4.classList.toggle('expandindo');
+    expandindo4.classList.toggle('active');
+    divisoria4.classList.toggle('active');
+});
+
+const expandir5 = document.querySelector('.expandir-icon-container5');
+const expandindo5 = document.querySelector('.expandir-icon5');
+const mural5 = document.querySelector('.mural5');
+const divisoria5 = document.querySelector('.divisoria5');
+
+expandir5.addEventListener('click', () => {
+    mural5.classList.toggle('expandindo');
+    expandindo5.classList.toggle('active');
+    divisoria5.classList.toggle('active');
 });
